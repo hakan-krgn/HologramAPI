f7020a9f6cb6997768812967743dfb57545c416e
Hakan <hakankargin_2003@hotmail.com>
2020-12-28 10:16:02 +0300

    Create hologram

--- src/main/java/com/hakan/hologram/listeners/TeleportListener.java
+++ src/main/java/com/hakan/hologram/listeners/TeleportListener.java
@@ -6,6 +6,7 @@ import com.hakan.hologram.hologram.Hologram;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -15,6 +16,25 @@ public class TeleportListener implements Listener {
 
     @EventHandler
     public void onTeleport(PlayerTeleportEvent event) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Player player = event.getPlayer();
+                UUID playerUniqueId = player.getUniqueId();
+                for (Hologram hologram : HologramAPI.getHolograms()) {
+                    for (UUID uuid : hologram.getPlayers()) {
+                        if (playerUniqueId.equals(uuid)) {
+                            hologram.update();
+                            break;
+                        }
+                    }
+                }
+            }
+        }.runTaskLater(Main.instance, 15);
+    }
+
+    @EventHandler
+    public void onWorldChange(PlayerChangedWorldEvent event) {
         new BukkitRunnable() {
             @Override
             public void run() {
